56beb7f6f8e9cb4f033b1114a1b783d170a28ca0
Carton <mrcartonbox@gmail.com>
2022-04-30 23:44:43 +0400

    testing response with json

--- Backend/api/routes/testAPI.js
+++ Backend/api/routes/testAPI.js
@@ -1,8 +1,15 @@
 var express = require("express");
 var router = express.Router();
 
+const responceData = {
+    message: "Test",
+    status: "Tired",
+    num: 420
+}
+
 router.get("/", function(req, res, next){
-    res.send("I hate prolog so much");
+    /*res.send("I hate prolog so much");*/
+    res.send(JSON.stringify(responceData));
 });
 
 module.exports = router;
--- testAPI.py
+++ testAPI.py
@@ -1,4 +1,5 @@
+
 import requests
 # There is nothing here but regret. See you on March 32
-response = requests.get("http://localhost:9000/api/v1/tests").content
-print(response)
+response = requests.get("http://localhost:9000/api/v1/tests")
+print(response.json()["message"])
