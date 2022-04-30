fc8130e990072207a8fd0991adc9c953b1020322
Carton <mrcartonbox@gmail.com>
2022-04-30 23:28:15 +0400

    testing api

--- Backend/api/routes/testAPI.js
+++ Backend/api/routes/testAPI.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res, next){
-    res.send("I hate prolog");
+    res.send("I hate prolog so much");
 });
 
 module.exports = router;
--- testAPI.py
+++ testAPI.py
@@ -1,3 +1,4 @@
 import requests
 # There is nothing here but regret. See you on March 32
-response = requests.get("http://localhost:9000")
+response = requests.get("http://localhost:9000/api/v1/tests").content
+print(response)
