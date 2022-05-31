2a8fdfb568c6fc67da55f9b8a108e8be03af226b
FuryPotat0 <oleg.bakharev.00@mail.ru>
2022-05-31 21:25:44 +0400

    add node_modules

---
+++ Frontend/client/my-app/src/long_descriptions/Cthulhu.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+export default class Cthulhu extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
